599ac48f0f3b8f3b9e509fbc248c59b17cbc8bd4
michanIDE <init.complete@gmail.com>
2025-01-04 23:43:34 +0900

    Modified MixinCachedRecipe

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Warning, more than 56 Speed Upgrades cause TPSs drop.
 Only on certain machine, test by yourself
+
--- src/main/java/dev/felnull/mekanismtweaks/mixin/MixinCachedRecipe.java
+++ src/main/java/dev/felnull/mekanismtweaks/mixin/MixinCachedRecipe.java
@@ -1,26 +1,32 @@
 package dev.felnull.mekanismtweaks.mixin;
 
 
-import dev.felnull.mekanismtweaks.Temp;
+import mekanism.api.recipes.MekanismRecipe;
 import mekanism.api.recipes.cache.CachedRecipe;
+// import mekanism.common.tile.prefab.TileEntityProgressMachine;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.function.IntSupplier;
 
 @Mixin(value = CachedRecipe.class, remap = false)
-public abstract class MixinCachedRecipe {
+public abstract class MixinCachedRecipe<RECIPE extends MekanismRecipe> {
 
     @Shadow public abstract void process();
 
+    @Shadow private IntSupplier baselineMaxOperations;
     @Shadow private IntSupplier requiredTicks;
 
-    @Inject(method = "process", at = @At(value = "INVOKE", target = "Lmekanism/api/recipes/cache/CachedRecipe;finishProcessing(I)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void injected(CallbackInfo ci, int operations) {
-        Temp.inject.accept(requiredTicks.getAsInt(), this::process);
+    @Inject(method = "process", at = @At("HEAD"))
+    private void setbaselineMaxOperations(CallbackInfo ci) {
+        int requiredTicksInt = requiredTicks.getAsInt();
+        if(requiredTicksInt < 0){
+            int reqTime = -requiredTicksInt;
+            baselineMaxOperations = () -> reqTime;
+        }
     }
 }
